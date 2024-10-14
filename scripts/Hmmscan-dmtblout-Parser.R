e8c6525b2a0e87f58f2f901e2b32dba0c41b244e
fairliereese <fairliek@comcast.net>
2024-10-14 15:51:17 +0200

    update w/ rubens code to parse hmmer

---
+++ scripts/Hmmscan-dmtblout-Parser.R
@@ -0,0 +1,71 @@
+#!/usr/bin/env Rscript
+
+## Hmmscan Domtblout Parser
+## This script utilises functions from the rhmmer CRAN package [https://github.com/arendsee/rhmmer/issues]
+
+suppressPackageStartupMessages(require("optparse"))
+
+option_list = list(
+  make_option(
+    c("-i", "--hmmscan_tbl"),
+    action = "store",
+    default = NA,
+    type = 'character',
+    help = 'Hmmscan output in --domtblout format'
+  ),
+  make_option(
+    c("-r", "--rhmmer_path"),
+    action = "store",
+    default = NA,
+    type = 'character',
+    help = 'Path to the "rhmmer.R" script (rhmmer CRAN package), from which we are using hmmscan output parsing functions.'
+  ),
+  make_option(
+    c("-o", "--output_table"),
+    action = "store",
+    default = NA,
+    type = 'character',
+    help = 'Output table containing json info'
+  )
+)
+
+opt <- parse_args(OptionParser(option_list=option_list))
+
+
+suppressPackageStartupMessages(require(magrittr))
+
+# 2) Coerce tbl to df
+tbl.to.df <- function(tb){
+  ## Coerce hmmscan parsed tbl (from 'read_domtblout()') to df. Replenish with NAs if tb is empty
+  # If empty tb
+  if(nrow(tb) == 0){
+    # create empty df
+    df <- as.data.frame(setNames(rep(list(NA), length(names(tb))), names(tb)))
+    df[["pfam_match"]] <- FALSE
+  } else {
+    df <- data.frame(tb)
+    df[["pfam_match"]] <- TRUE
+  }
+  return(df)
+}
+
+# 3) [NOT RUNNING FOR NOW - NOT CLEAR, Some fractions are above 1, Potential wrong formula] Calculate domain coverage
+## In addition, this can be included in a data analysis step, it is out of the scope of a simple 'parser'
+frac.alig.domain <- function(df){
+  ## Calculate the fraction of AA of the HMM model spanned by the PFAM alignment
+  ## This is to address Partiality in PFAM alignments. Based on the idea that an alignment of the query sequence (or a part of the query sequence) against 100% of the PFAM domain can be considered a functional domain. Whereas an alignment of 30% of the domain, can't.
+  df[["partiality"]] = (df[["hmm_to"]] + 1 - df[["hmm_from"]])/df[["domain_len"]]
+  return(df)
+}
+
+## RUN ##
+# 0. rhmmer (CRAN) functions
+source(opt$rhmmer_path)
+# 1. Parser HMMScan Output Table
+hmmscan.tb <- read_domtblout(opt$hmmscan_tbl)
+# 2. Coerce to df without filtering for transcript_id
+hmmscan.df <- tbl.to.df(hmmscan.tb)
+# 3. Add fraction of alignment
+hmmscan.df <- frac.alig.domain(hmmscan.df)
+# 4. Save the concatenated results as TSV
+write.table(hmmscan.df, file = opt$output_table, quote = F, sep = "\t", row.names = F)
