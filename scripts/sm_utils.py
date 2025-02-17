7492242e993758782ce3cc220ee649b9add8208c
fairliereese <fairliek@comcast.net>
2025-02-17 10:35:34 +0100

    update w/ snakemake code to get novel exon parts

--- scripts/sm_utils.py
+++ scripts/sm_utils.py
@@ -282,15 +282,15 @@ def get_internal_exon_info(gtf, ref_gtf):
 def get_novel_part_exons(ref_gtf, gtf,
                          exon_info_file):
     def make_exon_id(df):
-    """
-    Get an ID for each unique exon using
-    Chromosome, Strand, Start, End
-    """
-    df['eid'] = df['Chromosome'].astype(str)+'_'+\
-                df['Strand'].astype(str)+'_'+\
-                df['Start'].astype(str)+'_'+\
-                df['End'].astype(str)
-    return df
+        """
+        Get an ID for each unique exon using
+        Chromosome, Strand, Start, End
+        """
+        df['eid'] = df['Chromosome'].astype(str)+'_'+\
+                    df['Strand'].astype(str)+'_'+\
+                    df['Start'].astype(str)+'_'+\
+                    df['End'].astype(str)
+        return df
 
     # PODER
     df = pr.read_gtf(gtf).df
